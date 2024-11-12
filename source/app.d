03c74efa1241afbd8ecaba5118c8f8fe07d6aec9
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-11-11 20:05:32 -0500

    eod

--- source/app.d
+++ source/app.d
@@ -9,26 +9,27 @@ void main() {
     // The application loop
     bool running = true;
     bool white;    //R,G,B,A
-    gfx.palette[0]=0x0000FFFF;
     gfx.palette[1]=0x00FF00FF;
     gfx.palette[2]=0xFF0000FF;
     gfx.palette[3]=0xFFFFFFFF;
-   float s=0;
+    float s=0;
+    Sprite sp;
+    sp.pixels[]=2;
+    sp.x=100;
+    sp.y=100;
+    //sp.rotate(45);
     for (int i;running;i++) {
-           long start=SDL_GetTicks();
-        if((i%2)==0){
-            for(int x=0;x<320;x++){
-                for(int j=0;j<240;j++){
-                    gfx.pixels[((j*320)+x)]=cast(ubyte)uniform(0, 3);
-                }
-            }
-        }
-        if((gfx.events.length>0)&&(gfx.events[gfx.events.length-1]==SDL_QUIT)){
+        long start=SDL_GetTicks();
+        gfx.pixels[]=3;
+       
+        if((gfx.events.length>0)&&(gfx.events[gfx.events.length-1]=="QuitEvent")){
             running=false;
         }
+        
+         sp.draw(gfx);
         gfx.loop();
         s+=(cast(float)1000/(cast(float)(SDL_GetTicks()-start)));
-        writef("FPS: %f\n",s/cast(float)i);
+        //writef("FPS: %f\n",s/cast(float)i);
         
     } 
 
--- source/rel.d
+++ source/rel.d
@@ -7,7 +7,7 @@ class GFX{
     ubyte[] pixels;
     bool running;
     uint[2] dims;
-    SDL_EventType[] events; 
+    string[] events; 
     string[] errors;
     uint[255] palette;
     uint[] renderBuffer;
@@ -18,23 +18,56 @@ class GFX{
         this.window=new dsdl2.Window(name,[0,0],dims, false,false,false,false,true,false,false,true);
         this.pixels=new ubyte[320*240];
         this.renderSurface=new dsdl2.Surface([320,240],dsdl2.PixelFormat.rgba8888);
+        this.palette[0]=0x000000;
     }
     void loop(){
         dsdl2.pumpEvents();
         while (auto event = dsdl2.pollEvent()) {
-            events~=event.sdlEventType();
+            events~=event.toString().replace("dsdl2.","").replace("()","");
         }
          string err=SDL_GetError().to!string;
         if(err!=""){
             errors~=err;
         }
         foreach(i,ubyte pix;pixels){
+            if(pix!=0){
              (cast(uint*)this.renderSurface.buffer)[i]=palette[pix];
+             pixels[i]=0;
+            }
         }
         window.surface.blitScaled(renderSurface,dsdl2.Rect(0,0,window.width,window.height));
         window.update();
     }
     ~this(){
          dsdl2.quit();
+    }
+}
+struct Sprite{
+    ubyte[2] dims=[16,16];
+    ubyte[] pixels=new ubyte[16*16];
+    uint x;
+    uint y;
+    void draw(GFX gfx){
+        foreach(i,ubyte pix;pixels){
+            gfx.pixels[(((i/dims[0])+this.y)*320)+((i%dims[1])+this.x)]=pix;
+        }
+    }
+    void rotate(float angle){
+        ubyte[] newPixels=new ubyte[32*32];
+        float pi=3.1415926;
+        float rad=angle*(pi/180);
+        if(rad>2*pi)rad=0;
+        float sinr=rad.sin;
+        float cosr=rad.cos;
+        foreach(int i,ubyte pix;this.pixels){
+            int x=i%dims[0];
+            int y=i/dims[0];
+            float nx=16+(x*cosr)+(y*sinr);
+            float ny=16+(sinr*x)-(cosr*y);
+            writeln(x," ",y," ",cast(int)nx," ",cast(int)ny);
+            newPixels[cast(ulong)(nx+(ny*32))]=pix;
+        }    
+        this.pixels=newPixels;
+        this.dims=[32,32];
     }
 }
